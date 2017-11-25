cec5d91bfa6a90c21787246b39a68b7bf9fce788
CarstenKarbach <c.karbach.apps@gmail.com>
2017-11-25 11:13:49 +0100

    Add simple test with phpunit

---
+++ tests/EventManagementTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace jards\eventsapi\tests;
+
+use PHPUnit\Framework\TestCase;
+use jards\eventsapi\EventManagement;
+
+class EventManagementTest extends TestCase{
+
+	public function testGetEvents()
+	{
+		echo "testGetEvents\n";
+		
+		$eventManagement = new EventManagement();
+		$events = $eventManagement->getEvents();
+		
+		$content = $events->getContent();
+		
+		$eventsArray = json_decode($content);
+		$this->assertTrue(is_array($eventsArray) || is_empty($eventsArray), "Result of event management is not an array." );
+	}
+	
+}
